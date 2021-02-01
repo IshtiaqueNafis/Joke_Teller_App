cf58842dfd26961d39614b56c8056fc9608ccf07
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-02-01 01:29:05 -0500

    added button enable and disable file

--- script.js
+++ script.js
@@ -17,12 +17,17 @@ function tellMe(joke){
     });
 }
 
+//disable or enable button
 
+function toogleButton(){
+    button.disabled = !button.disabled // if its true will be false if its false will be true
+
+}
 
 async function getJokes(){
     let joke = ''; // emty cause it starts as empty
 
-    const apiUrl = 'https://v2.jokeapi.dev/joke/Any';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming';
     try {
     const response = await fetch(apiUrl); // get the data.
     const data = await response.json(); // this becomes the data converted to json
@@ -31,11 +36,16 @@ async function getJokes(){
     }else {
         joke = data.joke; // get the single line of joke
     }
+    // converts text to speech
     tellMe(joke)
+        // disables button
+      toogleButton() // button is diabled when the joke is done.
     }catch(error){
         //catch errors
         console.log('whhops',error)
     }
 }
 
-getJokes();
+// event listenrs
+button.addEventListener('click',getJokes); // pas the jokes functtion
+audioElement.addEventListener('ended',toogleButton)
