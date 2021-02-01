9ea86b3cbcdad09257ebe49e9af4e91d958219bd
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-02-01 01:11:36 -0500

    added translation

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const audioElement = document.getElementById('audio');
 
 
 const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
-function test(){
+// passing joke to voice rss api
+function tellMe(joke){
     VoiceRSS.speech({
         key: 'f577a5000e5b4752b6d009e6a9c7efb7',
-        src: 'Hello, world!',
+        src: joke,
         hl: 'en-us',
         v: 'Linda',
         r: 0,
@@ -16,20 +17,25 @@ function test(){
     });
 }
 
+
+
 async function getJokes(){
-    let joke = '';
+    let joke = ''; // emty cause it starts as empty
 
     const apiUrl = 'https://v2.jokeapi.dev/joke/Any';
     try {
     const response = await fetch(apiUrl); // get the data.
-    const data = await response.json; // this becomes the data converted to json
+    const data = await response.json(); // this becomes the data converted to json
     if(data.setup){ // check whether is data avilable.
         joke = `${data.setup} ... ${data.delivery}`; // this whehther data has two parts
     }else {
         joke = data.joke; // get the single line of joke
     }
+    tellMe(joke)
     }catch(error){
         //catch errors
         console.log('whhops',error)
     }
 }
+
+getJokes();
